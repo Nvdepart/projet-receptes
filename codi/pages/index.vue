81234ee464ca209e4c49b10911f0b4172c5f6a98
Nvdepart <67013480+Nvdepart@users.noreply.github.com>
2022-11-30 14:44:39 +0100

    receptes update

--- codi/pages/index.vue
+++ codi/pages/index.vue
@@ -1,36 +1,39 @@
 <template>
-          <v-container>
-            <v-row>
-              <v-col>
-                <h1>Home</h1>
-                <nuxt-link to="/receptes"></nuxt-link> 
-              </v-col>
-            </v-row>
-          </v-container>
+  <v-container>
+    <v-row>
+      <v-col cols="12">
+        <h1 style="font-size: 10vw">COKTAIL<span style="font-weight: 100;font-style: italic">TIME</span></h1>
+        <v-row>
+          <v-col cols="3" v-for="recepta in receptes" :key="recepta.idDrink">
+            <v-img :src="recepta.strDrinkThumb">
+              <v-container style="height: 100%" fill-height>
+                <v-row style="height: 100%" >
+                  <v-col align="end" justify="end">
+                    <h3>{{recepta.strDrink}}</h3>
+                  </v-col>
+                </v-row>
+              </v-container>
+            </v-img>
+          </v-col>
+        </v-row>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
+
 <script>
+import {receptes} from '@/dades/receptes'
 export default{
-            mounted(){
-              const axios = require("axios");
-
-                    const options = {
-                      method: 'GET',
-                      url: 'https://the-cocktail-db.p.rapidapi.com/list.php',
-                      params: {c: 'list'},
-                      headers: {
-                        'X-RapidAPI-Key': '8f3b633174msh296321d2dd558d0p18770fjsn41eb33976e62',
-                        'X-RapidAPI-Host': 'the-cocktail-db.p.rapidapi.com'
-                      }
-                    };
-
-                    axios.request(options).then(function (response) {
-                      console.log(response.data);
-                    }).catch(function (error) {
-                      console.error(error);
-                    });
-            }
-
+  mounted(){
+    console.log(receptes)
+    this.receptes = receptes
+    // Utilitzem les dades del plugin receptes
+    //console.log(this.$receptes)
+  },
+  data(){
+    return {
+      receptes:[]
+    }
+  }
 }
-
-
 </script>
